f1021e7a3ad2ff094b21b257a160fd38a8fe782b
Gabriel Romanini de Barros <gabriel.romanini.barros@gmail.com>
2023-07-28 14:52:17 -0300

    Started py

--- nn.py
+++ nn.py
@@ -1,6 +1,14 @@
 import random
+import numpy as np
 
-weights = [random.random()] * 16
-print(weights)
-print('hello')
-x1 = [0,0,1,0,1,0,1,1,1,1,1,0,0,0,0,0,0,0,1,0,1,0,1,1,0,1,1,1,1]
+
+# Each of the 784 pixels
+input = [0] * 784
+
+# Digits from 0-9
+output = [ [] * 16 ] * 10
+
+# Hidden layers
+f1 = [ [0] * 784 ] * 16
+f2 = [ [0] * 16 ] * 16
+
