eb96206be7efe6293d0113c2c0abfb04a2f21fe0
Gabriel Romanini de Barros <gabriel.romanini.barros@gmail.com>
2023-07-28 15:23:27 -0300

    Read in MNIST database

--- nn.py
+++ nn.py
@@ -1,6 +1,16 @@
 import random
 import numpy as np
+import tf as tf
+import keras
 
+#tf.keras.datasets.mnist.load_data(path="mnist.npz")
+
+
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+assert x_train.shape == (60000, 28, 28)
+assert x_test.shape == (10000, 28, 28)
+assert y_train.shape == (60000,)
+assert y_test.shape == (10000,)
 
 # Each of the 784 pixels
 input = [0] * 784
@@ -12,3 +22,4 @@ output = [ [] * 16 ] * 10
 f1 = [ [0] * 784 ] * 16
 f2 = [ [0] * 16 ] * 16
 
+print(x_train[1])
